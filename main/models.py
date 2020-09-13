0af39cfebeb5b92c16f724e78e9aed7b01741674
Eyu-Tes <eyoabtesfaye10@gmail.com>
2020-09-13 18:02:42 +0300

    added additional fields to models

--- main/models.py
+++ main/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
+# Create your models here.                                    
+class ProductTag(models.Model):
+    # products = models.ManyToManyField(Product, blank=True)
+    name = models.CharField(max_length=32)
+    slug = models.CharField(max_length=48)
+    description = models.TextField(blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
 # managers
 class ActiveManager(models.Manager):
     # So we can use: Product.objects.active() to return only active products
@@ -8,7 +20,6 @@ class ActiveManager(models.Manager):
         return self.filter(active=True)
 
 
-# Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=32)
     description = models.TextField(blank=True)
@@ -16,24 +27,19 @@ class Product(models.Model):
     # DecimalField must define max_digits & decimal_places
     price = models.DecimalField(max_digits=6, decimal_places=2)
     slug = models.SlugField(max_length=48)
+    tags = models.ManyToManyField(ProductTag, blank=True)
     active = models.BooleanField(default=True)
     in_stock = models.BooleanField(default=True)
-    data_updated = models.DateTimeField(auto_now=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     # custom manager
     objects = ActiveManager()
+
+    def __str__(self):
+        return self.name
 
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='product-images')
     thumbnail = models.ImageField(upload_to='product-thumbnails', null=True)
-
-
-class ProductTag(models.Model):
-    products = models.ManyToManyField(Product, blank=True)
-    name = models.CharField(max_length=32)
-    slug = models.CharField(max_length=48)
-    description = models.TextField(blank=True)
-    active = models.BooleanField(default=True)
-    
