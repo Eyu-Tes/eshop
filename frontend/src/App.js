3bf5112034fe8d89e790892e1fb483eaa35b7de3
Eyoab Tesfaye <eyoabtesfaye10@gmail.com>
2021-03-16 12:01:26 +0300

    Product detail page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Container } from 'react-bootstrap'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 import Home from './components/pages/Home'
-import Product from './components/pages/Product'
+import ProductDetail from './components/pages/ProductDetail'
 
 const App = () => {
   return (
@@ -12,7 +12,11 @@ const App = () => {
         <main className='py-3'>
           <Container>
             <Route exact path='/' component={Home} />
-            <Route path='/product/:id' component={Product} />
+            <Route path='/product/:id' render={props => 
+              <ProductDetail 
+                {...props}
+              />
+            } />
           </Container>
         </main>
         <Footer/>
