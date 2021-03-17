31520b0deb5e038c26e08e3f5071e07f199ff020
Eyoab Tesfaye <eyoabtesfaye10@gmail.com>
2021-03-17 19:48:27 +0300

    Custom error handler

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const colors = require('colors')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 
 /* ===== This needs to loaded before other configurations ===== */
@@ -19,6 +20,10 @@ app.get('/', (req, res) => {
 
 // load routers
 app.use('/api/products', require('./routes/products'))
+
+// error middlewares
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
